7d0ebe95db4f48e4203e493e2a9e4c15ed39849c
riku-nihon-univ <chiri21082@g.nihon-u.ac.jp>
2024-10-09 13:37:06 +0900

    編集

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+        <h1 className="title">Birdwatching</h1>
+        </header>
+    );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import Article from "./Article";
+import Aside from "./Aside";
+
+
+export default function Main() {
+    return(
+        <main>
+            <Article />
+            <Aside />
+        </main>
+    );
+}
